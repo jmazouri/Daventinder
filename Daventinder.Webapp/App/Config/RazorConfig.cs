d6c0393c9ab2f18ee582127f721eb9aa3270d5d4
John Mazouri <jmazouri@gmail.com>
2015-09-01 19:57:43 -0400

    Added test view + Razor config
    Added material design light css/js/template
    Added initial data display

---
+++ Daventinder.Webapp/App/Config/RazorConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.ViewEngines.Razor;
+
+namespace Daventinder.Webapp.App.Config
+{
+    public class RazorConfig : IRazorConfiguration
+    {
+        public IEnumerable<string> GetAssemblyNames()
+        {
+            yield return "Daventinder.Shared";
+        }
+
+        public IEnumerable<string> GetDefaultNamespaces()
+        {
+            yield return "Daventinder.Webapp";
+            yield return "Daventinder.Shared";
+        }
+
+        public bool AutoIncludeModelNamespace
+        {
+            get { return true; }
+        }
+    }
+}
--- Daventinder.Webapp/App/Modules/HomeModule.cs
+++ Daventinder.Webapp/App/Modules/HomeModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Daventinder.Shared;
 using Nancy;
 
 namespace Daventinder.Webapp.App.Modules
@@ -11,7 +12,10 @@ namespace Daventinder.Webapp.App.Modules
     {
         public HomeModule()
         {
-            Get["/"] = x => View["Home"];
+            DiningWebpageParser parser = new DiningWebpageParser("http://www.davenport.edu/dining/dining-hall/weeks-menu");
+            MenuPdfParser pdfParser = new MenuPdfParser(parser.PdfList);
+
+            Get["/"] = x => View["Home", pdfParser];
         }
     }
 }
